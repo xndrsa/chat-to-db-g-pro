4cf4f9087cc4549f666adec5c1a108a4c1703daa
xndrsa <51030823+xndrsa@users.noreply.github.com>
2024-11-11 15:36:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ api_key = os.getenv("API_KEY")
 
 
 conn =psycopg2.connect(
-    host=os.getenv("host"),
-  user=os.getenv("user"),
-  password=os.getenv("password"),
-  database=os.getenv("database")
+    host=os.getenv("HOST"),
+  user=os.getenv("USER"),
+  password=os.getenv("PASSWORD"),
+  database=os.getenv("DATABASE")
 )
 
 
