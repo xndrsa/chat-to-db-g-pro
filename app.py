14faaf6a83461d611abc09ccaf57a46db3d33e90
VarDux <alex.vargas@databuddies.com>
2024-11-11 15:57:24 -0600

    add verification

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ load_dotenv()
 
 
 api_key = os.getenv("API_KEY")
-
+print("DATABASE_HOST:", os.getenv("HOST"))
+print("api:", os.getenv("API_KEY"))
 
 conn =psycopg2.connect(
-    host=os.getenv("host"),
-  user=os.getenv("user"),
-  password=os.getenv("password"),
-  database=os.getenv("database")
+    host=os.getenv("HOST"),
+  user=os.getenv("USER"),
+  password=os.getenv("PASSWORD"),
+  database=os.getenv("DATABASE"),
+  port=5432
 )
 
 
